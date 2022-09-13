fe485eec59c31e044f6685ec5fd4b4cd63e9664e
mostapha <mossa5776@gmail.com>
2022-09-13 22:53:20 +0100

    Ajoutez des fichiers projet.

---
+++ Assets/enimy.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class enimy : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = waypoints.points[0]; 
+    }
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime,Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = waypoints.points[wavepointIndex];
+    }
+}
